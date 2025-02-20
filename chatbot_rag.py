c1f237fc1b46a1527ca8873257d700209daca98e
roxanaNik <roxananiksefat@trentu.ca>
2025-02-20 17:45:13 -0500

    some new comments

--- chatbot_rag.py
+++ chatbot_rag.py
@@ -16,7 +16,7 @@ load_dotenv()
 
 st.title("Chatbot")
 
-# initialize pinecone database
+# initialize pinecone databases
 pc = Pinecone(api_key=os.environ.get("PINECONE_API_KEY"))
 
 # initialize pinecone database
