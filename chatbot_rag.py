a9494870e0c12825cd23a52bd06ca0030707d9ea
roxananiksefat <roxananiksefat@trentu.ca>
2025-02-24 11:10:06 -0500

    some comment changes

--- chatbot_rag.py
+++ chatbot_rag.py
@@ -1,4 +1,4 @@
-#import streamlit libaries
+#import streamlit libary
 import streamlit as st
 import os
 from dotenv import load_dotenv
@@ -20,14 +20,14 @@ st.title("Chatbot")
 pc = Pinecone(api_key=os.environ.get("PINECONE_API_KEY"))
 
 # initialize pinecone database
-index_name = os.environ.get("PINECONE_INDEX_NAME")  # change if desired
+index_name = os.environ.get("PINECONE_INDEX_NAME")  # change if desired  jj
 index = pc.Index(index_name)
 
 # initialize embeddings model + vector store
 embeddings = OpenAIEmbeddings(model="text-embedding-3-large",api_key=os.environ.get("OPENAI_API_KEY"))
 vector_store = PineconeVectorStore(index=index, embedding=embeddings)
 
-# initialize chat history
+# initialize chat history mm
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
